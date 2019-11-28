267173666d179e15d411ef4151e4e4211dd6cc12
kim young jo <kimyoungjo@kimui-MacBook-Pro.local>
2019-11-28 13:30:08 +0900

    알맹이 ㄴ

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+import Facebook from './Facebook';
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <div className="footer-copyright">
+              Copyright 2019 © Kim Young Jo
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
